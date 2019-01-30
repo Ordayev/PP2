050af42007ab366c9c451f79fa94c4be55b2c19b
Ordayev <a.ordayev@gmail.com>
2019-01-31 00:23:54 +0600

    FIrst task

---
+++ Week1/task1/task1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+           // string s = Console.ReadLine(); //Reading First input 
+            //int n = int.Parse(s); // Converting string to int
+            int[] a = new int[n]; // Creating array
+            string s = Console.ReadLine();// reading secong line of input
+            string[] arr = s.Split();// Putting numbers into array
+            int cnt = 0;
+            List<int> p = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = int.Parse(arr[i]); 
+                for(int j = 2; j <= a[i]/2; j++)
+                {
+                    if(a[i]%j==0)
+                    {
+                        cnt++;
+                    }
+                }
+                if (cnt == 0)
+                {
+                    p.Add(a[i]);
+                }
+                cnt = 0;
+            }
+            for(int i= 0; i < p.Count(); i++)
+            {
+                if(p[i]!=1)
+                Console.Write(p[i] + " ");
+            }
+        }
+    }
+}
