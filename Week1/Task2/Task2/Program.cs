ff8fdac1614a70b9da1fbb138e2949f280842ca2
Ordayev <a.ordayev@gmail.com>
2019-01-31 00:57:29 +0600

    3 task is complete

---
+++ Week1/Task2/Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Student
+    {
+        public string name;
+        public string id;
+        public int yos;
+
+        public Student()
+        {
+            name = Console.ReadLine();
+            id = Console.ReadLine();
+            yos = Convert.ToInt16(Console.ReadLine());
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            Console.WriteLine(s);
+        }
+    }
+    
+}
---
+++ Week1/Task3/Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string s = Console.ReadLine();
+            string[] arr = s.Split();
+            int[] a = new int[n * 2];
+            for(int i = 0; i <n; i++)
+            {
+                a[i*2] = int.Parse(arr[i]);
+                a[i * 2 + 1] = a[i * 2];
+            }
+            for (int i = 0; i<n*2;i++)
+            {
+                Console.Write(a[i] + " ");
+            }
+        }
+    }
+}
