157ede38822c3354fb75a621232a80dee9034f1f
Ordayev <a.ordayev@gmail.com>
2019-02-06 00:45:15 +0600

    Task1 Week2

---
+++ Week2/Task1/Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool check = false;
+            string text = System.IO.File.ReadAllText(@"C:\Users\acer\Desktop\PP2\Week2\Task1\cin.txt");
+            for(int i= 0, j = text.Length-1; i < text.Length/2; i++, j--)
+            {
+                if (text[i] != text[j])
+                {
+                    check = true;
+                    break;
+                }
+            }
+            if(check == true)
+            {
+                Console.WriteLine("No");
+            }
+            else
+            {
+                Console.WriteLine("Yes");
+            }
+                      
+        } 
+    }
+}
